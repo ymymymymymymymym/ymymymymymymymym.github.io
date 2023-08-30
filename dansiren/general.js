e9be5f80510725fb9671fe5ccbc7e9eb2bdfc53a
ykmsms <142349518+ykmsms@users.noreply.github.com>
2023-08-30 22:15:17 +0900

    loadContent -> loadItem

--- dansiren/general.js
+++ dansiren/general.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const elements = ["header", "totop", "home", "groups", "members", "pairs"];
     elements.forEach(element => {
-        loadContent(`https://ykmsms.github.io/dansiren/parts/${element}.html`, element);
+        //loadContent(`https://ykmsms.github.io/dansiren/parts/${element}.html`, element);
+        loadItem(element)
     });
 });
 
 //パーツの一括読み込み
 function loadItem(parts) {
     loadContent(`https://ykmsms.github.io/dansiren/parts/${parts}.html`, parts);
-    const head = document.querySelector(parts);
-    const partsCSS = document.createElement("link");
-    partsCSS.rel = "stylesheet";
-    partsCSS.href = `https://ykmsms.github.io/dansiren/parts/${parts}.css`;
-    head.appendChild(partsCSS);
     const body = document.querySelector("body");
     const partsJS = document.createElement("script");
     partsJS.src = `https://ykmsms.github.io/dansiren/parts/${parts}.js`;
