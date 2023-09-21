fbf1ae746e4649c95a14add0d12460c9b56746d7
ykmsms <142349518+ykmsms@users.noreply.github.com>
2023-09-21 15:23:03 +0100

    header css

--- dansiren/parts/header.js
+++ dansiren/parts/header.js
@@ -1,10 +1,12 @@
 window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
     if(window.pageYOffset > 10){
-        header.style.opacity = '0.7';
+        //header.style.opacity = '0.7';
+        header.style.backgroundColor ='rgba(255,255,255,0.7)';
         header.style.backdropFilter = 'blur(10px)';
     }else if(window.pageYOffset < 200){
-        header.style.opacity = '1';
+        //header.style.opacity = '1';
+        header.style.backgroundColor ='rgba(255,255,255,1)';
         header.style.backdropFilter = NaN;
     }
 });
