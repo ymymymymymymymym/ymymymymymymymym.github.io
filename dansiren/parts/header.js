1d6ac6d0ab7813b80c9886594b073e68996b2087
ykmsms <142349518+ykmsms@users.noreply.github.com>
2023-09-21 13:39:45 +0100

    Update header.js

--- dansiren/parts/header.js
+++ dansiren/parts/header.js
@@ -1,8 +1,10 @@
 window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
     if(window.pageYOffset > 10){
-        header.style.opacity = '0.7';
+        //header.style.opacity = '0.7';
+        header.style.backdropFilter = 'blur(10px)';
     }else if(window.pageYOffset < 200){
-        header.style.opacity = '1';
+        //header.style.opacity = '1';
+        header.style.backdropFilter = 'blur(0px)';
     }
 });
