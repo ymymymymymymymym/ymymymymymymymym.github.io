053d6df0dda587b819d35947b82a52b48a9c523d
ykmsms <142349518+ykmsms@users.noreply.github.com>
2023-09-22 14:32:01 +0100

    Update header.js

--- dansiren/parts/header.js
+++ dansiren/parts/header.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function() {
     if(window.pageYOffset > 10){
         //header.style.opacity = '0.7';
         header.style.backgroundColor ='rgba(255,255,255,0.5)';
-        header.style.backdropFilter = 'blur(10px)';
+        header.style.backdropFilter = 'blur(5px)';
     }else if(window.pageYOffset < 200){
         //header.style.opacity = '1';
         header.style.backgroundColor ='rgba(255,255,255,1)';
